90e4e2d5b8ba885b56a044e22928ae68539b91ef
Noor Moghul <39253543+noormoghul@users.noreply.github.com>
2018-09-30 19:45:38 -0400

    Add files via upload

---
+++ prog1_part1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+
+{
+    if(argc == 1){
+        cout<<"NO PHRASE LENGTH"<<endl;
+        return 0;
+    }
+    string nStr(argv[1]);
+    int nInt = atoi(nStr.c_str());
+    if(nInt < 1){
+        cout<<"INVALID PHRASE LENGTH"<<endl;
+        return 0;
+    }
+    
+    if(argc == 2){
+        cout<<"NO MODE"<<endl;
+        return 0;
+    }
+    string mode(argv[2]);
+    if(!(mode=="all" || mode=="top")){
+        cout<<"INVALID MODE"<<endl;
+        return 0;
+    }
+    
+    
+    if(argc == 3){
+        cout<<"NO FILES GIVEN"<<endl;
+        return 0;
+    }
+    
+    
+    
+    for(int cc = 3;cc<argc;cc++){
+        
+        ifstream infile;
+        infile.open(argv[cc]);
+        
+        
+        
+        if(infile.is_open() == false){
+            cout<<"BAD FILE "<<argv[cc]<<endl;
+            return 0;
+        }
+        
+        
+    }
+    
+    
+    
+    return 0;
+    
+}
